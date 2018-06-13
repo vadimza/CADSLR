f9d8973271e258b9c69e4daf6defbf0fec5ccaf2
Ilia Rasskazov <il.rasskazov@gmail.com>
2018-06-12 21:23:17 -0500

    another reorganization

---
+++ example/run.bat
@@ -0,0 +1,11 @@
+ START CADSLR.exe 400.inp
+ START CADSLR.exe 401.inp             
+ START CADSLR.exe 402.inp             
+ START /WAIT CADSLR.exe 403.inp
+ START CADSLR.exe 404.inp
+ START CADSLR.exe 405.inp             
+ START CADSLR.exe 406.inp             
+ START /WAIT CADSLR.exe 407.inp
+ START CADSLR.exe 408.inp
+ START CADSLR.exe 409.inp             
+ START CADSLR.exe 410.inp             

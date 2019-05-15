4db08159045891f998a4cfc90d0b40e126436a45
Ilia Rasskazov <il.rasskazov@gmail.com>
2019-05-15 12:04:39 +0700

    updated example

--- example/run.bat
+++ example/run.bat
@@ -1,21 +1,6 @@
  START CADSLR.exe 10_10_540_400.inp                                 
  START CADSLR.exe 10_10_540_405.inp                                 
  START CADSLR.exe 10_10_540_410.inp                                 
- START /WAIT CADSLR.exe 10_10_540_415.inp                                 
+ START CADSLR.exe 10_10_540_415.inp                                 
  START CADSLR.exe 10_10_540_420.inp                                 
- START CADSLR.exe 10_10_540_425.inp                                 
- START CADSLR.exe 10_10_540_430.inp                                 
- START /WAIT CADSLR.exe 10_10_540_435.inp                                 
- START CADSLR.exe 10_10_540_440.inp                                 
- START CADSLR.exe 10_10_540_445.inp                                 
- START CADSLR.exe 10_10_540_450.inp                                 
- START /WAIT CADSLR.exe 10_10_540_455.inp                                 
- START CADSLR.exe 10_10_540_460.inp                                 
- START CADSLR.exe 10_10_540_465.inp                                 
- START CADSLR.exe 10_10_540_470.inp                                 
- START /WAIT CADSLR.exe 10_10_540_475.inp                                 
- START CADSLR.exe 10_10_540_480.inp                                 
- START CADSLR.exe 10_10_540_485.inp                                 
- START CADSLR.exe 10_10_540_490.inp                                 
- START /WAIT CADSLR.exe 10_10_540_495.inp                                 
- START CADSLR.exe 10_10_540_500.inp                                 
+ START CADSLR.exe 10_10_540_425.inp                                                                  
